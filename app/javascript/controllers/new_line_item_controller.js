c2dfa13c0d26b67d001e07db24d5f25e09b25291
Eric Roos <ericroos@hey.com>
2022-12-12 20:28:02 -0600

    Refactor to prevent nil reference bug
    Use built in stimulus callback methods to change when we add the event
    listeners to the new line item component

--- app/javascript/controllers/new_line_item_controller.js
+++ app/javascript/controllers/new_line_item_controller.js
@@ -2,19 +2,22 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ['form', 'loader', 'input', 'dropform']
-  connect(){
-    this.element.addEventListener('turbo:before-fetch-request', (ev) => {
-      this.loaderTarget.classList.remove('hidden')
-      this.formTarget.querySelectorAll("input").forEach( i => i.setAttribute("disabled", ""))
-    });
 
-    this.inputTarget.addEventListener('dragover',(ev) => ev.preventDefault());
-    this.inputTarget.addEventListener('drop',(ev) => {
+  inputTargetConnected(element){
+    element.addEventListener('dragover',(ev) => ev.preventDefault());
+    element.addEventListener('drop',(ev) => {
       ev.preventDefault();
       const updateUrl = '/line_items/' + ev.dataTransfer.getData('id')+'?aside=false';
       this.dropformTarget.setAttribute('action', updateUrl);
       this.dropformTarget.requestSubmit();
     })
+  }
+
+  connect(){
+    this.element.addEventListener('turbo:before-fetch-request', (ev) => {
+      this.loaderTarget.classList.remove('hidden')
+      this.formTarget.querySelectorAll("input").forEach( i => i.setAttribute("disabled", ""))
+    });
   }
 
   submitForm(){
