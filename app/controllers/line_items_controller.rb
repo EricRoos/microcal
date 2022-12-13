bdd7aa91e00971e90e11e8eb60a1a22cca24252b
Eric Roos <ericroos@hey.com>
2022-12-12 23:00:34 -0600

    Adds start time to line item and front end

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -9,6 +9,7 @@ class LineItemsController < ApplicationController
 
   def edit
     @line_item = LineItem.find(params[:id])
+    @line_item.start_time = @line_item.start_time.in_time_zone(Time.zone) if @line_item.start_time
     authorize @line_item
   end
 
@@ -42,6 +43,15 @@ class LineItemsController < ApplicationController
   end
 
   def update_line_item_params
-    params.require(:line_item).permit(:text, :completed, :description, :time_to_complete_minutes, :date)
+    params.require(:line_item).permit(:text, :completed, :description, :time_to_complete_minutes, :date,
+                                      :start_time).tap do |p|
+      if p[:start_time]
+        start_time = @line_item.date.to_time.in_time_zone
+        sent_time = p[:start_time].to_time
+        start_time = start_time.change({ hour: sent_time.hour, min: sent_time.min })
+        # start_time.zone = session['timezone']
+        p[:start_time] = start_time.utc
+      end
+    end
   end
 end
