2055cb475651cf7282fb0654c8a6d6658ac4cdc7
Eric Roos <ericroos@hey.com>
2022-12-20 15:53:33 -0600

    test: adds line item spec tests

--- spec/support/sign_in_helper.rb
+++ spec/support/sign_in_helper.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 module SignInHelper
-  def sign_in(user)
+  def sign_in(user=nil)
+    user ||= User.create(username: SecureRandom.hex, password: 'test123456')
     visit new_user_session_path
     fill_in 'Username', with: user.username
     fill_in 'Password', with: 'test123456'
     click_on 'Log in'
+    user
   end
 end
